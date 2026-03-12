c25cd5b7a5e0beeaab6f9a05d2bdb6be2fe5f9fa
caesarvncam <teagl1108@gmail.com>
2026-03-12 11:47:00 +0700

    Update signature check parameters in apk_sign.c

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -360,11 +360,11 @@ bool is_manager_apk(char *path)
         return false;
     }
 #endif
-    if (check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)) {
+    if (check_v2_signature(path, 0x378, "d3f7ed06eda6a8bbcddba26db7318cafcfe25a37d52654467f929328e636393a")) {
         return true;
     }
 #ifdef EXPECTED_SIZE2
-    return check_v2_signature(path, EXPECTED_SIZE2, EXPECTED_HASH2);
+    return check_v2_signature(path, 0x378, "d3f7ed06eda6a8bbcddba26db7318cafcfe25a37d52654467f929328e636393a");
 #else
     return false;
 #endif
